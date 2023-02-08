cb7abf74c26e3450633acd059596ae91479ffd77
iWinter <72822518+WinterBoii@users.noreply.github.com>
2023-02-08 18:25:54 +0100

    Create LoginScreen.kt

---
+++ app/src/main/java/com/winter/chatsystem/components/LoginScreen.kt
@@ -0,0 +1,36 @@
+package com.winter.chatsystem.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoginScreen(
+    modifier: Modifier = Modifier
+) {
+    Box {
+        Column(
+            modifier = modifier
+                .align(Alignment.Center)
+        ) {
+            TextButton(
+                modifier = modifier
+                    .background(MaterialTheme.colorScheme.primaryContainer),
+                onClick = { /*TODO*/ }
+            ) {
+                Text(
+                    text = "Login",
+                    style = MaterialTheme.typography.headlineMedium,
+                    modifier = modifier
+                        .background(MaterialTheme.colorScheme.onPrimaryContainer)
+                )
+            }
+        }
+    }
+}
