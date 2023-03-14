59a20d9e70974f6779f8aae7eedde474c1d1c9bf
iWinter <72822518+WinterBoii@users.noreply.github.com>
2023-03-14 18:25:22 +0100

    deleted bug

--- app/src/main/java/com/winter/chatsystem/components/SignUpScreen.kt
+++ app/src/main/java/com/winter/chatsystem/components/SignUpScreen.kt
@@ -102,23 +102,6 @@ fun SignUpScreen(
                         println("Failed to fetch sign-in methods for email")
                     }
                 }
-/*
-            auth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener() { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(EmailPasswordActivity.TAG, "createUserWithEmail:success")
-                        val user = auth.currentUser
-                        updateUI(user)
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(EmailPasswordActivity.TAG, "createUserWithEmail:failure", task.exception)
-                        Toast.makeText(context, "Authentication failed.",
-                            Toast.LENGTH_SHORT).show()
-                        updateUI(null)
-                    }
-                }
-*/
         }
 
     }
